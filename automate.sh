791ccfe93277b66aea46c88425e31eec3ce9fba3
Nico De Witte <nico.dewitte@vives.be>
2016-03-16 11:49:09 +0100

    Check to make sure script is running using root privs

--- automate.sh
+++ automate.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-# Resize the filesystem if '~/.racing_robots does not exist'
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# Resize the filesystem if '.racing_robots does not exist'
 cd /auto_mate
 if [ ! -f .racing_robots ]; then
 echo "Resizing filesystem ..."
-sudo fdisk /dev/mmcblk0 <<EOF
+fdisk /dev/mmcblk0 <<EOF
 d
 2
 n
@@ -16,22 +22,22 @@ w
 EOF
 touch .racing_robots
 echo "NEEDS_RESIZE" > .racing_robots
-sudo reboot
+reboot
 else
   RR=`cat .racing_robots`
   if [[ $RR == "NEEDS_RESIZE" ]]; then
     echo "Finalizing filesystem resize ..."
-    sudo resize2fs /dev/mmcblk0p2
+    resize2fs /dev/mmcblk0p2
     echo "DONE" > .racing_robots
 
     # Create some users
-    sudo adduser --quiet --disabled-password -shell /bin/bash --home /home/racing --gecos "Racing Robots" racing
-    echo -e 'robots\nrobots\n' | sudo passwd racing
+    adduser --quiet --disabled-password -shell /bin/bash --home /home/racing --gecos "Racing Robots" racing
+    echo -e 'robots\nrobots\n' | passwd racing
   fi
 
   # Install some packages
-  sudo apt-get update
-  sudo apt-get -y install git aptitude
+  apt-get update
+  apt-get -y install git aptitude
 fi
 
 echo "Done"
